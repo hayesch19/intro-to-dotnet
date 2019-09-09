e7f5b2faf2adbd34b95458a4e4a790d90cc3877e
hayesch19 <chrishayespro@gmail.com>
2019-09-09 13:00:39 -0400

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro_to_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
