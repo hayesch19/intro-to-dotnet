617a01861fed4d55c0321866726a6b5ccbba12e9
hayesch19 <chrishayespro@gmail.com>
2019-09-09 16:34:18 -0400

    V1 Explorer Mode Completed

--- Program.cs
+++ Program.cs
@@ -2,11 +2,46 @@
 
 namespace intro_to_dotnet
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var min = 1;
+      var max = 100;
+      var lower = min;
+      var upper = max;
+      var guess = 50;
+      var guessing = true;
+      Console.WriteLine("Think Of A Number Between 1-100");
+      while (guessing == true)
+      {
+        // Starting Guess
+        Console.WriteLine($"Is Your Number, {guess}?");
+        var input = Console.ReadLine();
+        // Guessed Correct
+        if (input == "yes")
         {
-            Console.WriteLine("Hello World!");
+          guessing = false;
         }
+        // If Higher
+        else
+        {
+          Console.WriteLine("Is It Higher or Lower?");
+          var high = Console.ReadLine();
+          if (high == "higher")
+          {
+            min = guess;
+            guess = (min + max) / 2;
+          }
+          // If Lower
+          else
+          {
+            max = guess;
+            guess = (min + max) / 2;
+          }
+        }
+      }
+      Console.WriteLine($"Congrats Your Number is {guess}");
     }
+  }
 }
