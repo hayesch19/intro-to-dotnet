4597f0fa51b15dc194c65eabfa7e99eb399bf993
hayesch19 <chrishayespro@gmail.com>
2019-09-09 19:11:23 -0400

    V3 Console Color Added/New Game Logic Fixed

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace intro_to_dotnet
   {
     static void Main(string[] args)
     {
+      Console.ForegroundColor = ConsoleColor.Blue;
       Console.WriteLine("I Can Guess Your Number!.");
       System.Threading.Thread.Sleep(2000);
+      Console.ResetColor();
+      Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("Think Of A Number Between 1-100 Then Hit 'Enter'");
       // user input
       while (Console.ReadKey().Key != ConsoleKey.Enter) { }
@@ -20,27 +23,36 @@ namespace intro_to_dotnet
       while (guessing == true)
       {
         // Starting Guess
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine($"Is Your Number, {guess}?");
         var input = Console.ReadLine();
         // Guessed Correct
         if (input == "yes")
         {
+          Console.ResetColor();
+          Console.ForegroundColor = ConsoleColor.Red;
           Console.WriteLine($"Haha Your Number is {guess}");
           System.Threading.Thread.Sleep(1000);
+          Console.ForegroundColor = ConsoleColor.Magenta;
           Console.WriteLine("Want To Play Again?");
           var playAgain = Console.ReadLine();
           if (playAgain == "yes")
           {
+            guess = 50;
             guessing = true;
           }
           if (playAgain == "no")
           {
             guessing = false;
+            Console.ResetColor();
           }
         }
         // If Higher
         else
         {
+          Console.ResetColor();
+          Console.ForegroundColor = ConsoleColor.Yellow;
           Console.WriteLine("Is It Higher or Lower?");
           var high = Console.ReadLine();
           if (high == "higher")
